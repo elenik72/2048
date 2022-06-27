5bb8750953d40592ddbcba89ea38e2efc87435c1
elenik72 <elenik1994@gmail.com>
2022-06-27 23:50:56 +0300

    feat: added new scripts

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,18 @@
-import css from "rollup-plugin-import-css"
+import css from 'rollup-plugin-import-css'
 import htmlTemplate from 'rollup-plugin-generate-html-template'
 import { terser } from 'rollup-plugin-terser'
 
 export default {
-  input: "src/app.js",
+  input: 'src/app.js',
   output: {
-    file: "dist/index.js",
+    file: 'dist/index.js'
   },
   plugins: [
     terser(),
     htmlTemplate({
       template: 'src/index.html',
-      target: 'index.html',
+      target: 'index.html'
     }),
-    css(),
+    css()
   ]
 }
